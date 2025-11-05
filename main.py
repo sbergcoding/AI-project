e009a56d129bc5be44106f1ff81a466cb1963468
sbergcoding <153482909+sbergcoding@users.noreply.github.com>
2025-11-04 18:46:22 -0800

    minor changes

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def main(): 
-
-    #I guess this is how we are going to start.
-    #use python -m venv myproject-env for compiling code
-    print("hello world")
-    main()
