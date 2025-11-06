56390df55918e3f1701966844eea07324d31a0f9
sbergcoding <153482909+sbergcoding@users.noreply.github.com>
2025-11-05 19:41:32 -0800

    small change so main function can do whatever

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import pandas as pd
+
+#print(sys.path)
+
+def main(): 
+
+    print("hello world")
+
+if __name__=="__main__":
+    main()
+    #I guess this is how we are going to start.
+    #use python -m venv myproject-env for compiling code
+    # then env\Scripts\activate to run
+    #to kill env type deactivate
+    #print("hello world")
+    
