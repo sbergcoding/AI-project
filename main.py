6fda8872113a758e2dc17c8fada121e8a8c7942d
sbergcoding <153482909+sbergcoding@users.noreply.github.com>
2025-11-05 20:01:43 -0800

    imported emotion detection database, make sure you guys read README.md

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
 import pandas as pd
+import kaggle
 
+kaggle.api.authenticate()
+
+kaggle.api.dataset_download_files('ananthu017/emotion-detection-fer', path='.',unzip=True)
 #print(sys.path)
 
 def main(): 
 
     print("hello world")
 
-if __name__=="__main__":
+if __name__=="__main__": #this is very important 
     main()
     #I guess this is how we are going to start.
     #use python -m venv myproject-env for compiling code
