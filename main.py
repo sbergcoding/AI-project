2cbfe8fb40c78665debfd354686be487301e2d90
sbergcoding <153482909+sbergcoding@users.noreply.github.com>
2025-11-09 16:20:17 -0800

    imported os and I think numpy was added

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import sys
+import numpy as np
 import pandas as pd
 import kaggle
+import os 
 
 kaggle.api.authenticate()
 
 kaggle.api.dataset_download_files('ananthu017/emotion-detection-fer', path='.',unzip=True)
+kaggle.api.dataset_metadata('ananthu017/emotion-detection-fer', path='.')
 #print(sys.path)
 
 def main(): 
 
     print("hello world")
+
+    
 
 if __name__=="__main__": #this is very important 
     main()
